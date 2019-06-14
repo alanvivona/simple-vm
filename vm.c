2da93304eda07be0529c40d4a87e53cd95864eda
Alan Vivona <alanvivona@users.noreply.github.com>
2019-06-14 17:54:18 -0300

    Update vm.c

--- vm.c
+++ vm.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <math.h>
+
+#include "vmisa.h"
 
 /* 
     Virtual machine description
@@ -9,7 +13,7 @@
         0x200   .text   size: 512b 0x200, perms: r-x
     
     CPU Registers:
-        r0-r9   16-bit general purpouse
+        r0-rF   16-bit general purpouse
         sp      16-bit stack pointer
         pc      16-bit program counter
         lr      16-bit link register
@@ -70,77 +74,127 @@
 
 */
 
-/* Virtual memory map */
-typedef struct vmem_section {
-    bool perms[3]{true, false, true};
-    char space[512];
-} vmem_section_text;
-
-typedef struct vmem_section {
-    bool perms[3]{true, true, false};
-    char space[512];
-} vmem_section_stack;
-
-typedef struct vmem { 
-    vmem_section_text   text;
-    vmem_section_stack  stack;
-} vmem;
-
-/* Virtual ISA implementation */
-int vstr(val, dst){
-    vmem.stack[dst] = val;    
-}
-
-int vldr(dst, src){
-    dst = vmem.stack[src];    
-}
-
-int vmov(dst, src){
-    dst = src;
-}
-
-int vadd(dst, opa, opb){
-    dst=opa+opb;
-}
-
-int veor(dst, opa, opb){
-    dst=opa^opb;
-}
-
-int vand(dst, opa, opb){
-    dst=opa&opb;
-}
-
-int vor(dst, opa, opb){
-    dst=opa|opb;
-}
-
-int vb(dst, offset){
-    vcpu.pc = dst;
-}            
+void printVMState(uint32_t* mem, uint32_t* gp_regs, uint32_t* pc){
+    printf("============================================\n");
+    printf("============= Current VM State =============\n");
+    printf("============================================\n");
     
-int vbl(dst, offset){
+    printf(">>>> Registers\n");
+    printf("pc: 0x%08x\n", *pc);
+    for (int i = 0; i < 2; i++) printf("0x%08x : 0x%08x\n", i, gp_regs[i]);
+    printf("============================================\n");
+    
+    printf(">>>> Memory\n");
+    for (int i = 0; i < 7; i++) printf("0x%08x : 0x%08x\n", i, mem[i]);
+    printf("============================================\n");
 
+    fflush(stdout);
 }
 
-int vret(){
-
+void printInstructionDetails(uint8_t* opcode, uint8_t* first_op, uint8_t* second_op, uint8_t* dst_op){
+    printf(">>>> ins:0x%02x op1:0x%02x op2:0x%02x dst:0x%02x\n", *opcode, *first_op, *second_op, *dst_op);
+    fflush(stdout);
 }
 
 int main(int argc, char const *argv[])
 {
+    uint32_t mem[7] = {
+        0x00000000, // vnop
+        0x01000000, // vadd r0, r1, r0
+        0x02000000, // vsub r0, r1, r0
+        0x03000000, // vand r0, r1, r0
+        0x04000000, // vor  r0, r1, r0
+        0x05000000, // veor r0, r1, r0
+        0x06000000, // vmov r0, r1
+    };
 
-    if (argc < 2)
+    uint32_t gp_regs[0x10] = {
+        0xffffffff, // r0
+        0x55555555, // r1
+        0x00000000, // r2
+        0x00000000, // r3
+        0x00000000, // r4
+        0x00000000, // r5
+        0x00000000, // r6
+        0x00000000, // r7
+        0x00000000, // r8
+        0x00000000, // r9
+        0x00000000, // rA
+        0x00000000, // rB
+        0x00000000, // rC
+        0x00000000, // rD
+        0x00000000, // rE
+        0x00000000, // rF
+    };
+
+    uint32_t pc = 0x00000000;
+    uint32_t sp = 0x00000000;
+    uint32_t lr = 0x00000000;
+
+    size_t instructions_q = sizeof(mem)/sizeof(mem[0]);
+    printf("Will execute %lu instructions\n", instructions_q);
+
+    printf("Printing Initial VM sate...\n");
+    printVMState(mem, gp_regs, &pc);
+
+    uint8_t executionState = 1;
+    while(executionState == 1)
     {
-        printf("Only %d arguments provided", argc);
-        return 0;
-    }
-    
-    for (size_t i = 0; i < sizeof(text_section); i++)
-    {
+        uint32_t opcode = mem[pc];
+        uint8_t instruction     = opcode >> 24;
+        uint8_t first_operand   = opcode >> 16 & 0x00ff;
+        uint8_t second_operand  = opcode >> 8  & 0x0000ff;
+        uint8_t destination     = opcode       & 0x000000ff;
+
+        printInstructionDetails(&instruction, &first_operand, &second_operand, &destination);
+
+        switch (instruction)
+        {
+            case 0x00:
+                printf("Performing vnop operation\n");
+                vnop();
+                break;
+           
+           case 0x01:
+                printf("Performing vadd operation\n");
+                vadd(gp_regs[0], gp_regs[1], &gp_regs[destination]);
+                break;
+
+            case 0x02:
+                printf("Performing vsub operation\n");
+                vsub(gp_regs[0], gp_regs[1], &gp_regs[destination]);
+                break;
+
+            case 0x03:
+                printf("Performing vand operation\n");
+                vand(gp_regs[0], gp_regs[1], &gp_regs[destination]);
+                break;
+
+            case 0x04:
+                printf("Performing vor operation\n");
+                vor(gp_regs[0], gp_regs[1], &gp_regs[destination]);
+                break;
+
+            case 0x05:
+                printf("Performing veor operation\n");
+                veor(gp_regs[0], gp_regs[1], &gp_regs[destination]);
+                break;
+
+            case 0x06:
+                printf("Performing vmov operation\n");
+                vmov(&gp_regs[0], &gp_regs[1]);
+                break;                                
+
+            default:
+                break;
+        }
         
+        pc++;
+        printVMState(mem, gp_regs, &pc);
+
+        if (pc >= instructions_q) executionState = 0;
     }
     
-    
+    printf("VM Execution ended!\n");
     return 0;
 }
