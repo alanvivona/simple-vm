ef721de09e45ce6f9ca322654517be976742cc9c
Alan Vivona <alan.g.vivona@gmail.com>
2020-04-24 22:30:02 +0200

    most operations are working!!

--- isa/isa.go
+++ isa/isa.go
@@ -2,7 +2,6 @@ package isa
 
 import (
 	"errors"
-	"strconv"
 
 	"../hardware"
 )
@@ -10,107 +9,290 @@ import (
 type Instructions map[byte]Executable
 
 type Executable interface {
-	Exec(h *hardware.VirtualHardware, args []byte) (bool, error)
+	Exec(h *hardware.VirtualHardware, args []byte) error
 }
 
 type Microcode struct {
-	Def        func(h *hardware.VirtualHardware, args []byte) (bool, error)
-	ArgsQ      uint
+	Def        func(h *hardware.VirtualHardware, args []byte) error
 	IsMemWrite bool
 }
 
-func (m Microcode) Exec(h *hardware.VirtualHardware, args []byte) (bool, error) {
+func (m Microcode) Exec(h *hardware.VirtualHardware, args []byte) error {
 	if m.IsMemWrite {
 		sp := h.CPU.Registers[h.CPU.SPIndex]
 		if uint(sp) >= uint(len(h.Mem)-8) {
-			return false, errors.New("Out of memory")
+			return errors.New("Out of memory")
 		}
 	}
-	keepGoing, err := m.Def(h, args)
-	h.CPU.Registers[h.CPU.PCIndex] += 3
-	return keepGoing, err
+	err := m.Def(h, args)
+	h.CPU.Registers[h.CPU.PCIndex] += InstructionSize
+	return err
 }
+
+const InstructionSize = 3
 
 func Create() Instructions {
 	return Instructions{
+		// Execution
 		0x00: start,
 		0xff: end,
-		//0x01: setr,
+
+		// Set registers
+		0x01: setr,
 		0xf1: setv,
-		//"put":   put,
-		//"get":   get,
+
+		// Stack
+		0x02: putr,
+		0xf2: putv,
+		0x03: get,
+
+		// Control flow
 		//"cmp":   cmp,
 		//"if":    ifExecute,
-		//"dec":   dec,
-		//"inc":   inc,
-		//"add":   add,
-		//"sub":   sub,
+
+		// Arithmetic
+		0xf6: addv,
+		0x06: addr,
+		0xf7: subv,
+		0x07: subr,
+		0x08: dec,
+		0x09: inc,
+
+		// Logic
+		0x0a: not,
+		0x0b: neg,
+		0x0c: and,
+		0x0d: or,
+		0x0e: xor,
+
+		// Subroutines
 		//"call":  call,
 		//"ret":   ret,
 	}
 }
 
+var not = Microcode{
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		v, err := h.CPU.Get(uint(bytecode[0]))
+		if err != nil {
+			return err
+		}
+		err = h.CPU.Set(uint(bytecode[0]), ^v)
+		if err != nil {
+			return err
+		}
+		return nil
+	},
+	IsMemWrite: false,
+}
+
+var neg = Microcode{
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		v, err := h.CPU.Get(uint(bytecode[0]))
+		if err != nil {
+			return err
+		}
+		err = h.CPU.Set(uint(bytecode[0]), (^v)+1)
+		if err != nil {
+			return err
+		}
+		return nil
+	},
+	IsMemWrite: false,
+}
+
+var or = Microcode{
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		v1, err := h.CPU.Get(uint(bytecode[0]))
+		if err != nil {
+			return err
+		}
+
+		v2, err := h.CPU.Get(uint(bytecode[1]))
+		if err != nil {
+			return err
+		}
+
+		err = h.CPU.Set(uint(bytecode[0]), v1|v2)
+		if err != nil {
+			return err
+		}
+		return nil
+	},
+	IsMemWrite: false,
+}
+
+var and = Microcode{
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		v1, err := h.CPU.Get(uint(bytecode[0]))
+		if err != nil {
+			return err
+		}
+
+		v2, err := h.CPU.Get(uint(bytecode[1]))
+		if err != nil {
+			return err
+		}
+
+		err = h.CPU.Set(uint(bytecode[0]), v1&v2)
+		if err != nil {
+			return err
+		}
+		return nil
+	},
+	IsMemWrite: false,
+}
+
+var xor = Microcode{
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		v1, err := h.CPU.Get(uint(bytecode[0]))
+		if err != nil {
+			return err
+		}
+
+		v2, err := h.CPU.Get(uint(bytecode[1]))
+		if err != nil {
+			return err
+		}
+
+		err = h.CPU.Set(uint(bytecode[0]), v1^v2)
+		if err != nil {
+			return err
+		}
+		return nil
+	},
+	IsMemWrite: false,
+}
+
 var start = Microcode{
-	Def: func(h *hardware.VirtualHardware, args []byte) (bool, error) {
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
 		for k := range h.CPU.Registers {
 			h.CPU.Registers[k] = 0
 		}
-		return true, nil
+		return nil
 	},
-	ArgsQ:      0,
 	IsMemWrite: false,
 }
 
 var end = Microcode{
-	Def: func(h *hardware.VirtualHardware, args []byte) (bool, error) {
-		return false, nil
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		return errors.New("End Execution")
 	},
-	ArgsQ:      0,
+	IsMemWrite: false,
+}
+
+var setr = Microcode{
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		v := h.CPU.Registers[uint(bytecode[1])]
+		return h.CPU.Set(uint(bytecode[0]), v)
+	},
 	IsMemWrite: false,
 }
 
 var setv = Microcode{
-	Def: func(h *hardware.VirtualHardware, args []byte) (bool, error) {
-		return true, h.CPU.Set(uint(args[0]), args[1])
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		return h.CPU.Set(uint(bytecode[0]), bytecode[1])
 	},
-	ArgsQ:      2,
 	IsMemWrite: false,
 }
 
-var putv = Microcode{
-	Def: func(h *hardware.VirtualHardware, args []byte) (bool, error) {
-		sp := h.CPU.Registers[h.CPU.SPIndex]
-		sp++
-		for i, v := range []byte(strconv.FormatInt(int64(args[0]), 16)) {
-			h.Mem[uint(sp)+uint(i)] = v
-		}
-		return true, nil
+var putr = Microcode{
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		v := h.CPU.Registers[uint(bytecode[0])]
+		return putv.Def(h, []byte{v})
 	},
-	ArgsQ:      2,
 	IsMemWrite: true,
 }
 
-//
-//func get(h *hardware.VirtualHardware, args []byte) (bool, error) {
-//
-//}
-//
-//func cmp(h *hardware.VirtualHardware, args []byte) (bool, error) {
-//
-//}
-//
-//func ifExecute(h *hardware.VirtualHardware, args []byte) (bool, error) {
-//
-//}
-//
-//func add(h *hardware.VirtualHardware, args []byte) (bool, error) {
-//
-//}
-//
-//func sub(h *hardware.VirtualHardware, args []byte) (bool, error) {
-//
-//}
-//
-//func dec(h *hardware.VirtualHardware, args []byte) (bool, error) {
-////sub(cpu, mem, args[0], 1)
-//}
+var putv = Microcode{
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		sp := h.CPU.Registers[h.CPU.SPIndex]
+		err := h.Mem.Set(int(sp), bytecode[0])
+		if err != nil {
+			return err
+		}
+		err = h.CPU.Set(h.CPU.SPIndex, sp+1)
+		if err != nil {
+			return err
+		}
+		return nil
+	},
+	IsMemWrite: true,
+}
+
+var get = Microcode{
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		sp, err := h.CPU.Get(h.CPU.SPIndex)
+		if err != nil {
+			return err
+		}
+		v, err := h.Mem.Get(int(sp) - 1)
+		if err != nil {
+			return err
+		}
+		err = h.CPU.Set(uint(bytecode[0]), v)
+		if err != nil {
+			return err
+		}
+		h.CPU.Set(h.CPU.SPIndex, sp-1)
+		return nil
+	},
+	IsMemWrite: false,
+}
+
+var addr = Microcode{
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		v, err := h.CPU.Get(uint(bytecode[1]))
+		if err != nil {
+			return err
+		}
+		addv.Def(h, []byte{bytecode[0], v})
+		return nil
+	},
+	IsMemWrite: false,
+}
+
+var addv = Microcode{
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		v, err := h.CPU.Get(uint(bytecode[0]))
+		if err != nil {
+			return err
+		}
+		h.CPU.Set(uint(bytecode[0]), v+bytecode[1])
+		return nil
+	},
+	IsMemWrite: false,
+}
+
+var subv = Microcode{
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		comp := (^bytecode[1]) + 0x01
+		return addv.Def(h, []byte{bytecode[0], comp})
+	},
+	IsMemWrite: false,
+}
+
+var subr = Microcode{
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		v, err := h.CPU.Get(uint(bytecode[1]))
+		if err != nil {
+			return err
+		}
+		return subv.Def(h, []byte{bytecode[0], v})
+	},
+	IsMemWrite: false,
+}
+
+var inc = Microcode{
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		return addv.Def(h, []byte{bytecode[0], 0x01})
+	},
+	IsMemWrite: false,
+}
+
+var dec = Microcode{
+	Def: func(h *hardware.VirtualHardware, bytecode []byte) error {
+		return subv.Def(h, []byte{bytecode[0], 0x01})
+	},
+	IsMemWrite: false,
+}
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
+
 	"./hardware"
 	"./isa"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	clearScreen()
 
 	vh, err := hardware.Create("fakeone", 48)
 	if err != nil {
@@ -15,23 +20,42 @@ func main() {
 	}
 	logrus.Infof("Created Hardware")
 	logrus.Infof("CPU: %s", vh.CPU.Model)
-	logrus.Infof("Memory: %d", len(vh.Mem))
+	logrus.Infof("Memory: %db", len(vh.Mem))
 
 	InstructionSet := isa.Create()
 
 	bytecode := []byte{
-		0x00, 0x00, 0x00, //"start"
-		0xf1, 0x00, 0x01,
-		0x01, 0x01, 0x00,
-		0xf1, 0x02, 0x02,
-		0x01, 0x03, 0x02,
-		0xff, 0x00, 0x00, //"end"
+		0x00, 0x00, 0x00, // start
+		0xf1, 0x00, 0x01, // set ra 0x01
+		0x01, 0x01, 0x00, // set rb ra
+		0xf1, 0x02, 0x02, // set rc 0x02
+		0x01, 0x03, 0x02, // set rd rc
+
+		0x02, 0x00, 0x00, // put ra
+		0xf2, 0x44, 0x00, // put 0x44
+		0x03, 0x00, 0x00, // get ra
+
+		0xf6, 0x03, 0x02, // add rd 0x44
+		0x06, 0x03, 0x02, // add rd ra
+		0xf7, 0x03, 0x02, // sub rd 0x44
+		0x07, 0x03, 0x02, // sub rd ra
+
+		0x08, 0x00, 0x00, // dec ra
+		0x09, 0x00, 0x00, // inc ra
+
+		0x0a, 0x00, 0x02, // not ra
+		0x0b, 0x00, 0x02, // neg ra
+		0x0c, 0x00, 0x01, // and ra rb
+		0x0d, 0x00, 0x01, // or  ra rb
+		0x0e, 0x00, 0x01, // xor ra rb
+
+		0xff, 0x00, 0x00, // end
 	}
 
 	for true {
 		pc := vh.CPU.Registers[vh.CPU.PCIndex]
-		if uint(pc) >= uint(len(bytecode)-3) {
-			logrus.Fatalf("No bytecode left. Early end of execution at pc=0x%02x", pc)
+		if uint(pc) >= uint(len(bytecode)-isa.InstructionSize) {
+			logrus.Fatalf("No bytecode left. End of execution at pc=0x%02x", pc)
 			break
 		}
 
@@ -41,9 +65,9 @@ func main() {
 			logrus.Fatalf("Unrecognized op identifier: 0x%x", opID)
 			break
 		}
-		args := bytecode[pc+1 : pc+3]
+		args := bytecode[pc+1 : pc+isa.InstructionSize]
 		logrus.Infof("Executing bytecode: 0x%02x 0x%02x 0x%02x", opID, args[0], args[1])
-		keepGoing, err := microcode.Exec(&vh, args)
+		err := microcode.Exec(&vh, args)
 
 		vh.CPU.Print()
 		vh.Mem.Print()
@@ -52,9 +76,16 @@ func main() {
 			logrus.Error(err)
 			break
 		}
-		if !keepGoing {
-			break
-		}
+
+		// Wait for user input to execute next instruction
+		fmt.Scanln()
+		clearScreen()
 	}
 	logrus.Infof("Code execution finished")
+}
+
+func clearScreen() {
+	c := exec.Command("clear")
+	c.Stdout = os.Stdout
+	c.Run()
 }
